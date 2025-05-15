99413101f7566a1b5c2bf47853fa96b6a078e7ad
chirag shetty <chiru.shetty@hotmail.com>
2025-05-15 20:00:11 +0100

    Starting to build the client side

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
